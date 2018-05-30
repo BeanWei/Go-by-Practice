cfd5f780ad58bacab56511ae909cd8c1bb715fab
BeanWei <157318439@qq.com>
2018-05-30 10:25:22 +0800

    :rocket:更新源码注释

--- Emoji/emoji.go
+++ Emoji/emoji.go
@@ -18,16 +18,21 @@ func CodeMap() map[string]string {
 	return emojiCodeMap
 }
 
-//匹配规则
+//匹配规则 对国旗字符表情单独处理
 var flagRegexp = regexp.MustCompile(":flag-(a-z{2}):")
 
 //字符与unicode字符匹配
 func emojize(x string) string {
+	//将传入的表情字符做键查询map对应的值
 	str, ok := emojiCodeMap[x]
 	if ok {
+		//如果找到对应的unicode字符则返回(这里在字符后面加个空格不影响，只是为了输出好看一点)
 		return str + ReplacePadding
 	}
+	//如果没有找到对应的则进行自定义规则匹配
+	//.FindStringSubmatch(x)返回一个 [][]string 数组，返回的数组如果是二维的说明匹配成功
 	if match := flagRegexp.FindStringSubmatch(x); len(match) == 2 {
+		//将两个字母分别转换成对应的unicode字符然后拼接起来
 		return regionlIndicator(match[1][0]) + regionlIndicator(match[1][1])
 	}
 	return x
@@ -51,11 +56,14 @@ func replaseEmoji(input *bytes.Buffer) string {
 		}
 		//如果第一个rune字符是 : ,并且此时emoji缓冲器长度是1
 		//这个循环遍历了传入的字符，emoji缓冲器长度是1且rune的头部是 : 的时候才刚刚开始遍历这个字符
+		/*这里是为了排除传入的表情符号前面是接着冒号的这种情况(eg:I like::apple:)*,出现这种情况则继续读取*/
 		if i == ':' && emoji.Len() == 1 {
 			return emoji.String() + replaseEmoji(input)
 		}
 		//上面情况都不满足的的话，则将 i 写入emoji
 		emoji.WriteRune(i)
+
+		//switch语句:都满足随机执行case，都不满足不执行case
 		switch {
 		//参考http://www.cnblogs.com/golove/p/3273585.html
 		// IsSpace 判断 r 是否为一个空白字符
@@ -63,31 +71,39 @@ func replaseEmoji(input *bytes.Buffer) string {
 		// 空格, U+0085 (NEL), U+00A0 (NBSP)
 		// 其它空白字符的定义有“类别 Z”和“Pattern_White_Space 属性”
 		case unicode.IsSpace(i):
-			//如果是空白字符则返回转换后的emoji
+			//如果是空白字符则说明不满足表情字符的要求则直接返回退出该函数
 			return emoji.String()
 		case i == ':':
-			//当此时 i == ':'的时候，整个input中的缓冲器的字符已经读取完成，返回匹配完成的unicode字符
+			//当此时 i == ':'的时候，一个完整的表情字符已经读取完成，返回匹配完成的unicode字符
 			return emojize(emoji.String())
 		}
 	}
 }
 
+//对传入的字符串做分割判断(分隔符判断标识符为 :: )
 func compile(x string) string {
+	//如果没有传入的字符串则直接返回""
 	if x == "" {
 		return ""
 	}
+	//将传入的字符放的缓冲器中并初始化
 	input := bytes.NewBufferString(x)
+	//初始化一个空字符串的缓冲器
 	output := bytes.NewBufferString("")
 
+	//遍历缓冲器中的每个rune字符
 	for {
 		i, _, err := input.ReadRune()
 		if err != nil {
 			break
 		}
+		//switch语句，不满足case则进行default操作
 		switch i {
 		default:
+			//默认操作:将i写入output缓冲器中
 			output.WriteRune(i)
 		case ':':
+			//如果当前字符为 : (也就是传入的表情字符分隔符)，则将input缓冲器做为参数传给replaseEmoji函数
 			output.WriteString(replaseEmoji(input))
 		}
 	}
@@ -97,8 +113,8 @@ func compile(x string) string {
 //*[]interface{} 空接口  接受任何类型
 func compileValues(a *[]interface{}) {
 	for i, x := range *a {
+		//str为传入的字符
 		if str, ok := x.(string); ok {
-			fmt.Println(str)
 			(*a)[i] = compile(str)
 		}
 	}
--- Emoji/emoji_test.go
+++ Emoji/emoji_test.go
@@ -1,6 +1,7 @@
 package emoji
 
 import (
+	"bytes"
 	"testing"
 )
 
@@ -8,14 +9,28 @@ const (
 	beerKey  = ":beer:"
 	beerText = " ビール!!!"
 	flag     = ":flag-us:"
+	likebeer = ":beer: Beer!!!"
 )
 
 var testFText = "test " + emojize(beerKey) + beerText
 var testText = emojize(beerKey) + beerText
 
 func TestPrintln(t *testing.T) {
-	_, err := Println(beerKey, beerText)
+	_, err := Println(beerKey, beerText, likebeer)
 	if err != nil {
 		t.Error("Println ", err)
+	}
+}
+
+func TestMultiColons(t *testing.T) {
+	var buf bytes.Buffer
+	_, err := Fprint(&buf, "A :smile: and another: :smile:")
+	if err != nil {
+		t.Error("Fprint ", err)
+	}
+
+	testCase := "A " + emojize(":smile:") + " and another: " + emojize(":smile:")
+	if buf.String() != testCase {
+		t.Error("Fprint ", buf.String(), "!=", testCase)
 	}
 }
